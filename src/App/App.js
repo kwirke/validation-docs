6ea690978cc06840253e691c99135683234e01c1
Kwirke <nebrios2@gmail.com>
2019-07-28 00:29:35 +0200

    Design layout

--- src/App/App.js
+++ src/App/App.js
@@ -2,7 +2,39 @@ import React from 'react'
 import styled from 'styled-components'
 import './App.css'
 import {Navbar} from '../Navbar/Navbar'
+import { deepKoamaru, eerieBlack } from '../Style/Colors';
+
+const Layout = styled.div`
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    height: 100vh;
+`
+
+const Body = styled.div`
+    display: flex;
+    flex-direction: row;
+    height: 100%;
+`
+
+const Sidebar = styled.div`
+    width: 333px;
+    height: 100%;
+    background-color: ${eerieBlack};
+`
+
+const Page = styled.div`
+    width: 100%;
+    height: 100%;
+    // background-color: ${eerieBlack};
+`
 
 export const App = () => (
-  <Navbar/>
+    <Layout>
+        <Navbar/>
+        <Body>
+            <Sidebar/>
+            <Page/>
+        </Body>
+    </Layout>
 )
